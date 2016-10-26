f9a6d87148c7af9fc3a792c04ad2044fa9528375
Md Jahidul Hamid <jahidulhamid@yahoo.com>
2016-10-26 16:05:02 +0600

    update

--- html/search/functions_a.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['_7eregex',['~Regex',['../classjpcre2_1_1select8_1_1Regex.html#a474c6ada8c2f86b221adf1e74d155b1c',1,'jpcre2::select8::Regex::~Regex()'],['../classjpcre2_1_1select16_1_1Regex.html#abc322bdcc742b0a93dfd54c4df03c5b3',1,'jpcre2::select16::Regex::~Regex()'],['../classjpcre2_1_1select32_1_1Regex.html#a2acbfc515f0cb09c9dfae39eee04a1fd',1,'jpcre2::select32::Regex::~Regex()']]]
-];
